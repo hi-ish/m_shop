677a6ae4ed1c1549b01f64c9b1cc2f7017657408
Ishan Shrestha <ishh2k77@gmail.com>
2020-10-27 13:39:59 +0545

    Add Delivery Address

--- m_shop/lib/Orders/placeOrderPayment.dart
+++ m_shop/lib/Orders/placeOrderPayment.dart
@@ -5,7 +5,12 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-class PaymentPage extends StatefulWidget {
+class PaymentPage extends StatefulWidget 
+{
+  final String addressId;
+  final double totalAmount;
+
+  PaymentPage({Key key, this.addressId, this.totalAmount}) : super(key: key);
   @override
   _PaymentPageState createState() => _PaymentPageState();
 }
