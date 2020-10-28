67e443c7ecd5ad1d4e290f8ec34b38ddae25f380
Ishan Shrestha <ishh2k77@gmail.com>
2020-10-28 20:55:17 +0545

    View orders placed

--- m_shop/lib/Orders/myOrders.dart
+++ m_shop/lib/Orders/myOrders.dart
@@ -16,7 +16,47 @@ class _MyOrdersState extends State<MyOrders> {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: Scaffold(),
+      child: Scaffold(
+        appBar: AppBar(
+          iconTheme: IconThemeData(color: Colors.white),
+          centerTitle: true,
+          title: Text('My Orders', style: TextStyle(color: Colors.white),),
+          actions: [
+            IconButton(icon: Icon(Icons.arrow_drop_down), color: Colors.white,
+             onPressed: ()
+             {
+               SystemNavigator.pop();
+             },)
+          ],
+        ),
+        body: StreamBuilder<QuerySnapshot>(stream: EcommerceApp.firestore
+        .collection(EcommerceApp.collectionUser)
+        .document(EcommerceApp.sharedPreferences.getString(EcommerceApp.userUID)).collection(EcommerceApp.collectionOrders).snapshots(),
+        builder: (c, snapshot)
+        {
+          return snapshot.hasData
+          ? ListView.builder(itemCount: snapshot.data.documents.length ,itemBuilder: (c, index)
+          {
+            return FutureBuilder<QuerySnapshot>(
+              future: Firestore.instance
+              .collection('items')
+              .where('shortInfo', whereIn: snapshot.data.documents[index].data[EcommerceApp.productID])
+              .getDocuments(),
+              builder: (c, snap)
+              {
+                return snap.hasData
+                ? OrderCard(
+                  itemCount: snap.data.documents.length,
+                  data: snap.data.documents,
+                  orderID: snapshot.data.documents[index].documentID,
+                )
+                :Center(child: circularProgress(),);
+              },
+            );
+          })
+          : Center(child: circularProgress(),);
+        }),
+      ),
     );
   }
 }
